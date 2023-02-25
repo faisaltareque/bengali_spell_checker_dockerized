e3879268ccd01fc14ab2b62bb33996166c4b3c93
Faisal Tareque <faisaltareque98@gmail.com>
2023-02-25 13:06:34 +0600

    added top candidated route

--- app/main.py
+++ app/main.py
@@ -16,11 +16,19 @@ app.add_middleware(
 
 class Word(BaseModel):
     word: str
+    
+class Word_rank(BaseModel):
+    word: str
+    top: int
 
 @app.get("/")
 def hello():
 	return {'Spell Checker': 'Send a POST request to /check with a word in the body to get a list of possible corrections'}
 
-@app.post("/check")
+@app.post("/candidates")
 def check_spelling(word: Word)->list:
     return jsonable_encoder(spell_checker.candidates(word.word))
+
+@app.post("/top_candidates")
+def check_spelling(word: Word_rank)->list:
+    return jsonable_encoder(spell_checker.top_K_candidates(word.word, word.top))
